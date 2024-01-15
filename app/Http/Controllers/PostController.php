a9c6db0474800150e61d0c4c6f0edd4d39f650ce
forta21 <vasya-andreev.1997@mail.ru>
2024-01-15 21:51:25 +0300

    создали мигрпацию, контроллер и модель для постов

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Collection
+    {
+        return Post::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View|Application|Factory
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        dd($request);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post): Post
+    {
+        return $post;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
