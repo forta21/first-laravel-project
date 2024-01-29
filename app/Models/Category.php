6331ccd4604dcdc4576d58278aabb42c5c06359d
forta21 <vasya-andreev.1997@mail.ru>
2024-01-29 21:26:55 +0300

    связь котегорий с постами

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @property int $id
@@ -15,4 +16,9 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function posts(): BelongsToMany
+    {
+        return $this->belongsToMany(Post::class);
+    }
 }
